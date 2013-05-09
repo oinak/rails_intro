8cb075138cd0df4b74a0727f097ca149bed8f2f7
Fernando Martínez <fernando.mdelacueva@gmail.com>
2013-05-09 20:45:47 +0200

    empezar

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curso::Application.config.secret_token = 'fb0e1a830286aceb7540f35da5f0a8a94995be4f9bf409cbdb15f336311317bd2734d7a4e6db45c63611d62ea23af3963fcd0f5e1f85ed6c3ad6762dd71107b0'
