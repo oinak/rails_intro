633ebf0db5a1ba1964ef51d4b92f6cc259258405
Fernando Martínez <fernando.mdelacueva@gmail.com>
2013-05-13 19:49:47 +0200

    Primer testeo

--- test/unit/course_test.rb
+++ test/unit/course_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  def test_titulo_no_puede_estar_vacio
+    course = courses(:one)
+    course.name = nil
+    assert !course.save, "sin titulo no deberia salvar"
+    assert !course.valid?
+    assert_not_nil course.errors[:name]
+
+  end
 end
