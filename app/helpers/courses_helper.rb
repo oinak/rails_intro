ccf6ab74df21ade30ccba92ec60434dc2e36c9bc
Fernando Martínez <fernando.mdelacueva@gmail.com>
2013-05-17 13:49:38 +0200

    Ejemplo de helper personalizado

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,12 @@
 module CoursesHelper
+
+  def link_to_editions(course)
+    ed_count = course.editions.size
+    if ed_count == 0
+      link_to("Add",new_course_edition_path(course))
+    else
+      link_to("View #{ed_count}", course_editions_path(course))
+    end
+  end
+
 end
