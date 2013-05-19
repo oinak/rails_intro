590908026441c060b60b30c5ac0bdcf632894f71
Fernando Martínez <fernando.mdelacueva@gmail.com>
2013-05-19 17:57:52 +0200

    Modelo User para authlogic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515091506) do
+ActiveRecord::Schema.define(:version => 20130519155628) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -33,5 +33,15 @@ ActiveRecord::Schema.define(:version => 20130515091506) do
   end
 
   add_index "editions", ["course_id"], :name => "index_editions_on_course_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
 
 end
